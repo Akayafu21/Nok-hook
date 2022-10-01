4cda15f330930be6df00d7d35efc1a405e2ac23e
Akayafu21 <110641474+Akayafu21@users.noreply.github.com>
2022-10-01 19:55:53 +0700

    Add files via upload

--- js/post.js
+++ js/post.js
@@ -3,57 +3,60 @@ liff.init({liffId:'1657437132-LVyE7X7o'});
 
 const submitbtn = document.getElementById('btnpost');
 
-
-// alert(localStorage.length);
+var flex = '[{"type": "flex","altText": "this is a flex message","contents": {"type": "bubble","direction": "ltr","header": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "โพสต์งาน"+localStorage[localStorage.length],"align": "center","contents": []}]},"footer": {"type": "box","layout": "horizontal","contents": [{"type": "button","action": {"type": "uri","label": "ส่งงาน","uri": uri_link},"color": "#FFE6AEFF"}]}}}]'
 
 //post btn
 submitbtn.addEventListener('click',()=>{
+  if(document.getElementById("name").value == ''){
+    alert('please input assignment name');
+  }else{
     var uri_link = "https://drive.google.com/drive/folders/1xeVYUJALWLxLVezp_bkDRHtDF4WepCip?usp=sharing";
     localStorage[localStorage.length] = document.getElementById("name").value;
-    liff.sendMessages([
-        {
-            "type": "flex",
-            "altText": "this is a flex message",
-            "contents": {
-              "type": "bubble",
-              "direction": "ltr",
-              "header": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "โพสต์งาน"+localStorage[localStorage.length],
-                    "align": "center",
-                    "contents": []
-                  }
-                ]
-              },
-              "footer": {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [
-                  {
-                    "type": "button",
-                    "action": {
-                      "type": "uri",
-                      "label": "ส่งงาน",
-                      "uri": uri_link
-                    },
-                    "color": "#FFE6AEFF"
-                  }
-                ]
-              }
-            }
-          }
-    ]);
+    liff.sendMessages(flex);
    
       alert('Message sent');
+    }
+  });
 
-});
 
-
-
+//   [
+//     {
+//         "type": "flex",
+//         "altText": "this is a flex message",
+//         "contents": {
+//           "type": "bubble",
+//           "direction": "ltr",
+//           "header": {
+//             "type": "box",
+//             "layout": "vertical",
+//             "contents": [
+//               {
+//                 "type": "text",
+//                 "text": "โพสต์งาน"+localStorage[localStorage.length],
+//                 "align": "center",
+//                 "contents": []
+//               }
+//             ]
+//           },
+//           "footer": {
+//             "type": "box",
+//             "layout": "horizontal",
+//             "contents": [
+//               {
+//                 "type": "button",
+//                 "action": {
+//                   "type": "uri",
+//                   "label": "ส่งงาน",
+//                   "uri": uri_link
+//                 },
+//                 "color": "#FFE6AEFF"
+//               }
+//             ]
+//           }
+//         }
+//       }
+      
+// ]
 
 
 
