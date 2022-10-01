c6343b33320be79da39f3ef35e8e727aed4e1b1f
Akayafu21 <110641474+Akayafu21@users.noreply.github.com>
2022-10-01 21:45:38 +0700

    Add files via upload

--- js/post.js
+++ js/post.js
@@ -3,20 +3,53 @@ liff.init({liffId:'1657437132-LVyE7X7o'});
 
 const submitbtn = document.getElementById('btnpost');
 
-var flex = '[{"type": "flex","altText": "this is a flex message","contents": {"type": "bubble","direction": "ltr","header": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "โพสต์งาน"+localStorage[localStorage.length],"align": "center","contents": []}]},"footer": {"type": "box","layout": "horizontal","contents": [{"type": "button","action": {"type": "uri","label": "ส่งงาน","uri": uri_link},"color": "#FFE6AEFF"}]}}}]'
+// var flex = '[{"type": "flex","altText": "this is a flex message","contents": {"type": "bubble","direction": "ltr","header": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "โพสต์งาน"+localStorage[localStorage.length],"align": "center","contents": []}]},"footer": {"type": "box","layout": "horizontal","contents": [{"type": "button","action": {"type": "uri","label": "ส่งงาน","uri": uri_link},"color": "#FFE6AEFF"}]}}}]'
 
 //post btn
 submitbtn.addEventListener('click',()=>{
-  if(document.getElementById("name").value == ''){
-    alert('please input assignment name');
-  }else{
     var uri_link = "https://drive.google.com/drive/folders/1xeVYUJALWLxLVezp_bkDRHtDF4WepCip?usp=sharing";
-    localStorage[localStorage.length-1] = document.getElementById("name").value;
-    liff.sendMessages(flex);
-   
+    // localStorage[localStorage.length-1] = document.getElementById("name").value;
+    liff.sendMessages(  [
+      {
+          "type": "flex",
+          "altText": "this is a flex message",
+          "contents": {
+            "type": "bubble",
+            "direction": "ltr",
+            "header": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "โพสต์งาน"+"localStorage[localStorage.length-1]",
+                  "align": "center",
+                  "contents": []
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "ส่งงาน",
+                    "uri": uri_link
+                  },
+                  "color": "#FFE6AEFF"
+                }
+              ]
+            }
+          }
+        }
+        
+  ]);
       alert('Message sent');
     }
-  });
+  );
 
 
 //   [
